e482b7dd3204753b2a3e2dd0c420ba326cab9d4e
Oleksandr <merlin.ua.1@gmail.com>
2023-12-16 21:02:52 +0200

    Prepare for deployment

--- app.js
+++ app.js
@@ -36,4 +36,11 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
+const port = process.env.PORT || 3001;
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
 module.exports = app;
